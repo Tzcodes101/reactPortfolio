5e2637988ec8605d82b104a3b406dd579b291497
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2021-01-01 12:51:19 -0500

    bootstrap added to index.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,5 +4,15 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+import NavBar from "./components/NavBar"; 
+import Footer from "./components/Footer";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
 
 //router with links to diff pages depending on click
+function NavBar() {
+
+}
+
+export default NavBar; 
