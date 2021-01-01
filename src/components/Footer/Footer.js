a138aee91b3707ee9c4767ae80150a50bc772b1d
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2021-01-01 13:33:13 -0500

    footer styling addition

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+    return (
+        <footer>
+            &#169; Copyright
+        </footer>
+    )
+}
+
+export default Footer;
