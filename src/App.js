b78e41cd279a8ef30f1b1ebdaaff35b560c81bad
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2021-01-01 12:46:23 -0500

    edited imports to App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+import NavBar from "./components/NavBar"; 
+import Footer from "./components/Footer";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
 
 function App() {
 
   //Router with switch for url to diff pages
-
   return (
     <Router>
       <NavBar />
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,8 @@
-
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 //router with links to diff pages depending on click
