b2c65faa7532f1de68d70269aea3b21a861273ac
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2021-01-01 13:45:25 -0500

    portfolio code addition

---
+++ src/pages/Portfolio/Portfolio.js
@@ -0,0 +1,66 @@
+import React from "react";
+import NavBar from "../../components/NavBar/NavBar";
+import Footer from "../../components/Footer/Footer";
+import "./pages.css";
+
+function Portfolio () {
+    return (
+        
+    // <!--Activate grid system-->
+    <div class="container">
+        <NavBar />
+        <div className="row">
+            {/* <!--Set responsive layout--> */}
+            <div className="col-lg-12 col-md-12 col-xs-12">
+
+                    {/* <!--Heading--> */}
+                    <section className="card-body">
+                        <h4 className="card-title" id="port-title">My Portfolio</h4>
+                    </section>
+
+                    {/* <!--Images, responsive to web browser via bootsrap classes--> */}
+                    <div className="row">
+
+
+                            <div className="card port-card" style="width: 18rem;">
+                                <img src="./Screenshots/eatDaBurger.jpg" class="card-img-top" alt="image of app" style="height: 8rem;"></img>
+                                <div className="card-body">
+                                  <h5 className="card-title">Eat Da Burger</h5>
+                                  <p className="card-text">An application enabling a user to add and devour certain burgers. Type in a custom-burger, have it appear on one side, choose to devour it, and watch it move! <br></br>
+                                    <a href="https://github.com/Tzcodes101/burger">Visit GitHub Repo.</a>
+                                  </p>
+                                  <a href="https://evening-harbor-99452.herokuapp.com/burgers" className="btn btn-dark">View App</a>
+                                </div>
+                              </div>
+
+                              <div className="card port-card" style="width: 18rem;">
+                                <img src="./Screenshots/a_welcome.png" className="card-img-top" alt="image of app" style="height: 8rem;"></img>
+                                <div className="card-body">
+                                  <h5 className="card-title">Safe Space</h5>
+                                  <p className="card-text">Cycle through inspirational quotes, choose sounds from a dropdown playlist, and search for soothing images, all with the goal of making a comfotable safe to process thoughts.<br></br>
+                                    <a href="https://github.com/Tzcodes101/Safe-Space">Visit GitHub Repo.</a>
+                                  </p>
+                                  <a href="https://tzcodes101.github.io/Safe-Space/" className="btn btn-dark">View App</a>
+                                </div>
+                              </div>
+
+                              <div className="card port-card" style="width: 18rem;">
+                                <img src="./Screenshots/employeeTracker.jpg" className="card-img-top" alt="image of app" style="height: 8rem;"></img>
+                                <div className="card-body">
+                                  <h5 className="card-title">Employee Tracker</h5>
+                                  <p className="card-text">A CLI application to manage employees. A user can add, veiw, or remove departments, roles, and employees, as well as update employee managers..<br></br>
+                                    <a href="https://github.com/Tzcodes101/employeeTracker">Visit GitHub Repo.</a></p>
+                                  <a href="https://drive.google.com/file/d/1Vy--kRTFCeRb2ODrGDQcH5TCDpO4ByTc/view" className="btn btn-dark">View App</a>
+                                </div>
+                              </div>
+                    </div>
+
+            </div>
+
+        </div>
+    <Footer />
+    </div>
+    )
+}
+
+export default Portfolio;
